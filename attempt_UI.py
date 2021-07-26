be73657a5fcb4720036f640bfd724597dd41ab79
Igneous <liu.igneous@gmail.com>
2021-07-25 20:22:54 -0500

    nothing

--- attempt_UI.py
+++ attempt_UI.py
@@ -1,2 +1,15 @@
 import PySimpleGUI as sg
-sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+import tkinter as tk
+sg.theme('DarkAmber')
+
+layout = [[sg.Text('hi')],
+[[sg.Text('Another row')], sg.InputText()],
+[sg.Button('ok'), sg.Button('cancel')] ]
+
+window = sg.Window('hi', layout)
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'cancel':
+        break
+    print('You entered', layout)
+
