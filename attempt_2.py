359e971cb5fbefdfd68c9d7e68024ab6cf3753ea
Igneous <liu.igneous@gmail.com>
2021-07-26 18:49:52 -0500

    Added branches for small yellow flowers

--- attempt_2.py
+++ attempt_2.py
@@ -1,3 +1,4 @@
+from PIL import Image
 class Trillium:
     color = "white"
     size = "ground cover"
@@ -30,44 +31,43 @@ class CommonStJohnsWort:
     color = "yellow"
     size = "knee height"
 
-flowers = [Trillium, Yarrow]
-
-white_flws = [Trillium, BloodRoot, LadySlipper,RueAnemone, Yarrow]
-white_gc_flws = [Trillium, RueAnemone, BloodRoot]
-
-yellow_flws = [SolomonsSeal, CommonMullien, CommonStJohnsWort]
+flowers = [Trillium, LadySlipper, BloodRoot, RueAnemone, Yarrow, SolomonsSeal, CommonStJohnsWort, CommonMullien]
 
 flw_colors = ["white", "yellow","orange", "blue/violet", "red/pink"]
 flw_sizes = ["ground cover", "small", "knee height", "waist height to tall"]
 flw_color = input(f"What color is you're flower? Options: {flw_colors}")
 flw_size = input(f"What size is you're flower? Options: {flw_sizes}")
-flw_petal_num = input(f"How many petals does you're flower have?")
-flw = []
-
 
 while True:
     if flw_color == "white":
-        flw.extend(white_flws)
         if flw_size == "ground cover":
-            flw.extend(white_gc_flws)
+            flw_petal_num = input(f"How many petals does you're flower have?")
             if flw_petal_num == "3" :
                 print("You're flower is a trillium!")
                 print(Trillium.properties)
+                break
             if flw_petal_num == "5":
                 print("You're flower is a Rue Anenome!")
                 print(RueAnemone.properties)
+                break
             if flw_petal_num == "8":
                 print("You're flower is Blood Root!")
-
+                break
         if flw_size == "knee height":
-            flw.extend(Yarrow)
             print("You're flower is Yarrow")
-
-        break
+            break
     if flw_color == "yellow":
-        flw.extend(yellow_flws)
-    if flw_size == "small":
-        flw.append(SolomonsSeal)
-        break
+        if flw_size == "small":
+            leaf_shape = input("What are the leaves of you're flower? (options: heart shaped, flat, jagged with three parts) ")
+        if leaf_shape == "heart shaped":
+                    print("You're flower is a Marsh Marigold")
+        if leaf_shape == "jagged with three parts":
+                    print("You're flower is a Swamp Buttercup")
+        if leaf_shape == "flat":
+            q = input("Does you're flower have 5 petals? [y/n]")
+            if q == "y":
+                print("You're flower is Clintonia")
+            else:
+                print("You're flower is Solomon's Seal")
 
 
