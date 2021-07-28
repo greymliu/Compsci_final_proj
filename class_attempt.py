d9a4dc7b88fa0ba20ecddd5d25e374937486bf32
Igneous <liu.igneous@gmail.com>
2021-07-27 20:16:59 -0500

    New attempt

---
+++ class_attempt.py
@@ -0,0 +1,15 @@
+class flower:
+    color = ""
+    size = ""
+    petal_shape =""
+    petal_num = 0
+    latin_binom =""
+
+    def __init__(self, argcolor, argsize, argpetal_shape, argpetal_num, arglatin_binom):
+        self.color = argcolor
+        self.size = argsize
+        self.petal_shape = argpetal_shape
+        self.petal_num = argpetal_num
+        self.arglatin_binom = arglatin_binom
+
+
