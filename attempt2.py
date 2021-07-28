767a8d9be4f8d305b487044148ac657956d42bbf
Igneous <liu.igneous@gmail.com>
2021-07-27 19:54:58 -0500

    purple flowers added

--- attempt2.py
+++ attempt2.py
@@ -1,37 +1,4 @@
-from PIL import Image
-class Trillium:
-    color = "white"
-    size = "ground cover"
-    properties = "xxx"
-    petal_shape = "rounded"
-    latin_binom = "xxx"
-    petal_num = 3
-    additional_info = "xxxx"
-class LadySlipper:
-    color = "white"
-    size = "knee height"
-    petal_shape = "cup-like"
-class BloodRoot:
-    color = "white"
-    size = "ground cover"
-class RueAnemone:
-    color = "white"
-    size = "ground cover"
-    properties = "xxx"
-class Yarrow:
-    color = "white"
-    size = "knee height"
-class SolomonsSeal:
-    color = "yellow"
-    size = "small"
-class CommonMullien:
-    color = "yellow"
-    size = "waist height to tall"
-class CommonStJohnsWort:
-    color = "yellow"
-    size = "knee height"
 
-flowers = [Trillium, LadySlipper, BloodRoot, RueAnemone, Yarrow, SolomonsSeal, CommonStJohnsWort, CommonMullien]
 flw_colors = ["white", "yellow","orange", "blue/violet", "red/pink"]
 flw_sizes = ["ground cover", "small", "knee height", "waist height to tall"]
 flw_color = input(f"What color is you're flower? Options: {flw_colors}")
@@ -43,11 +10,9 @@ while True:
             flw_petal_num = input(f"How many petals does you're flower have?")
             if flw_petal_num == "3" :
                 print("Your flower is a trillium!")
-                print(Trillium.properties)
                 break
             if flw_petal_num == "5":
                 print("Your flower is a Rue Anenome!")
-                print(RueAnemone.properties)
                 break
             if flw_petal_num == "8":
                 print("Your flower is Blood Root!")
@@ -75,10 +40,11 @@ while True:
                 break
         if flw_size == "knee height":
             leaf_shape = input('What is the leaf shape of your flower? Options:(small leaflets coming directly off the stem, long serrated leaflets)')
+            ##Add pics for leaf shapes!!!
             if leaf_shape == 'small leaflets coming directly off the stem':
                 print("You're flower is a Common St. John's Wort")
                 break
-            else:
+            if leaf_shape == "long serrated leflets":
                 print("Your flower is a Tansy")
                 break
         if flw_size == "waist height to tall":
@@ -94,6 +60,30 @@ while True:
             break
         if flw_size == "tall":
             print( "Your flower is a Tiger Lily")
+    if flw_color == "blue" or "violet" or "blue/violet":
+        flw_size = input("What size is your flower? Options: (small, knee height, waist height)")
+        if flw_size == "small":
+            print("Your flower is Corn Flower")
+            break
+        if flw_size == "knee height":
+            y_n = input("Do petals come off your flower's center?[y/n]")
+            if y_n == 'y':
+                print("Your flower is Echinacea")
+                break
+            else:
+                q_4 = input("Does")
+                #USE IMAGES DO DIFFER BETWEEN LOBELIA AND LUPINE
+                break
+        if flw_size == "knee height":
+            flw_shape = input("what shape is your flower? Options: (small off of a spike, large with three petals going outwards)")
+            if flw_shape == "small off of a spike":
+                print("Your flower is Blue Vervain")
+                break
+            else:
+                print("Your flower is an Iris")
+                break
+
+
 
     
 
